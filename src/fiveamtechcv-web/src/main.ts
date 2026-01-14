234bef3f234da6f751bb3355341f4882ad9de38c
Davide Plessi <davideplessi@fiveamtech.it>
2026-01-14 14:55:53 +0100

    css to scss refactor

--- src/fiveamtechcv-web/src/main.ts
+++ src/fiveamtechcv-web/src/main.ts
@@ -15,7 +15,7 @@ import { createApp } from 'vue'
 
 // Styles
 import 'unfonts.css'
-import '@/assets/main.css'
+import '@/styles/main.scss'
 import '@/styles/cyber.css'
 
 const app = createApp(App)
